73d398ebfce90ed12bf87b423e3b9bda55991f29
nathanyoung1 <nathan.young.wsu@gmail.com>
2017-06-01 12:23:08 -0700

    UserInterface

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Spreadsheet_NYoung
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.TheButton = new System.Windows.Forms.Button();
+            this.BGColor = new System.Windows.Forms.Button();
+            this.Undo = new System.Windows.Forms.Button();
+            this.Redo = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.Load = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(741, 652);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dataGridView1_CellBeginEdit);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // TheButton
+            // 
+            this.TheButton.Location = new System.Drawing.Point(109, 0);
+            this.TheButton.Name = "TheButton";
+            this.TheButton.Size = new System.Drawing.Size(75, 23);
+            this.TheButton.TabIndex = 1;
+            this.TheButton.Text = "TheButton";
+            this.TheButton.UseVisualStyleBackColor = true;
+            this.TheButton.Click += new System.EventHandler(this.TheButton_Click);
+            // 
+            // BGColor
+            // 
+            this.BGColor.Location = new System.Drawing.Point(0, 0);
+            this.BGColor.Name = "BGColor";
+            this.BGColor.Size = new System.Drawing.Size(103, 23);
+            this.BGColor.TabIndex = 2;
+            this.BGColor.Text = "Background Color";
+            this.BGColor.UseVisualStyleBackColor = true;
+            this.BGColor.Click += new System.EventHandler(this.BGColor_Click);
+            // 
+            // Undo
+            // 
+            this.Undo.Location = new System.Drawing.Point(190, 0);
+            this.Undo.Name = "Undo";
+            this.Undo.Size = new System.Drawing.Size(143, 23);
+            this.Undo.TabIndex = 3;
+            this.Undo.Text = "Undo";
+            this.Undo.UseVisualStyleBackColor = true;
+            this.Undo.Click += new System.EventHandler(this.Undo_Click);
+            // 
+            // Redo
+            // 
+            this.Redo.Location = new System.Drawing.Point(339, 0);
+            this.Redo.Name = "Redo";
+            this.Redo.Size = new System.Drawing.Size(182, 23);
+            this.Redo.TabIndex = 4;
+            this.Redo.Text = "Redo";
+            this.Redo.UseVisualStyleBackColor = true;
+            this.Redo.Click += new System.EventHandler(this.Redo_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(527, 1);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 5;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(608, 1);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(75, 23);
+            this.Load.TabIndex = 6;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 682);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Redo);
+            this.Controls.Add(this.Undo);
+            this.Controls.Add(this.BGColor);
+            this.Controls.Add(this.TheButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button TheButton;
+        private System.Windows.Forms.Button BGColor;
+        private System.Windows.Forms.Button Undo;
+        private System.Windows.Forms.Button Redo;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Load;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,218 @@
+﻿/*
+Nathan Young
+SID# 10932954
+CPTS 321
+HW 7
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SpreadsheetEngine;
+using CptS321;
+using System.IO;
+
+namespace Spreadsheet_NYoung
+{
+    public partial class Form1 : Form
+    {
+        Spreadsheet thesheet = null;
+
+        // **************************************************************************************************
+        // **************************** INITIALIZE FORM *****************************************************
+        // **************************************************************************************************
+
+
+        public Form1() // A spreadsheet 
+        {
+
+            InitializeComponent();
+            char header; // title for each column
+            for (int i = 65; i < 91; i++) // label columns
+            {
+                header = (char)i;
+                dataGridView1.Columns.Add(header.ToString(), header.ToString());
+            }
+            for (int i = 1; i < 51; i++) // label rows
+            {
+                DataGridViewRow newrow = new DataGridViewRow();
+                newrow.HeaderCell.Value = i.ToString(); // title for each row
+                dataGridView1.Rows.Add(newrow);
+            }
+            thesheet = new Spreadsheet(26, 50);
+            dataGridView1.RowHeadersWidth = 50;
+            thesheet.CellPropertyChanged += Thesheet_CellPropertyChanged; // subscribe to spreadsheet class
+            Redo.Enabled = false;
+            Undo.Enabled = false;
+        }
+
+
+
+        private void Thesheet_CellPropertyChanged(object sender, PropertyChangedEventArgs e) // UI/logic communication hub
+        {
+            if (e.PropertyName == "Value")
+            {
+                SpreadsheetCell thecaller = sender as SpreadsheetCell;
+                int column = thecaller.ColumnIndex;
+                int row = thecaller.RowIndex;
+                (dataGridView1[column, row]).Value = thecaller.Value;
+            }
+
+            if (e.PropertyName == "BGColor")
+            {
+                SpreadsheetCell thecaller = sender as SpreadsheetCell;
+                int column = thecaller.ColumnIndex;
+                int row = thecaller.RowIndex;
+                (dataGridView1[column, row]).Style.BackColor = System.Drawing.Color.FromArgb((int)thecaller.BGColor);
+            }
+        }
+
+        // useless object
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        // ********************************************* the button from HW3 ***********************************************
+
+        private void TheButton_Click(object sender, EventArgs e) // 
+        {
+            Random number = new Random();
+            for (int i = 0; i < 50; i++) // random
+            { thesheet.GetCell(number.Next(0, 25), number.Next(0, 49)).Text = "Hello?"; }
+
+            for (int i = 0; i < 49; i++) // label B
+            {
+                thesheet.GetCell(1, i).Text = "This is cell B" + (i + 1);
+                thesheet.GetCell(0, i).Text = "=B" + (i + 1);
+            }
+        }
+
+        // ********************************************* TEXT EDIT **********************************************************
+
+        private void dataGridView1_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e) // cell text begin edit
+        {
+            if (dataGridView1[e.ColumnIndex, e.RowIndex].Value != null)
+            {
+                SpreadsheetCell placeholder = thesheet.GetCell(e.ColumnIndex, e.RowIndex);
+                dataGridView1[e.ColumnIndex, e.RowIndex].Value = placeholder.Text;
+            }
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e) // cell text end edit
+        {
+            if (dataGridView1[e.ColumnIndex, e.RowIndex].Value != null)
+            {
+                SpreadsheetCell thecell = thesheet.GetCell(e.ColumnIndex, e.RowIndex);
+                thesheet.AddUndo(thecell, thecell.Text);                                 // log change in undo stack
+                Undo.Enabled = true;                                                     // enable undo button
+                thecell.Text = (string)dataGridView1[e.ColumnIndex, e.RowIndex].Value;   // update text
+                thesheet.ClearRedoStack();                                              // clear redo stack
+                this.Redo.Enabled = false;                                              // disable redo button
+                this.Undo.Text = "Undo Text Change";
+            }
+        }
+
+        // ******************************************* COLOR EDIT *********************************************************
+
+        private void BGColor_Click(object sender, EventArgs e) // change in Background color
+        {
+            System.Windows.Forms.ColorDialog changecolor = new ColorDialog();
+
+            if (changecolor.ShowDialog() == DialogResult.OK)                                        // opens dialog
+            {
+                SpreadsheetCell thecell = null;
+                SpreadsheetCell[] thelistofcells = new SpreadsheetCell[dataGridView1.SelectedCells.Count]; // all changed cells
+                int i = 0;
+                foreach (DataGridViewCell cell in dataGridView1.SelectedCells)                      // log cells current color
+                {
+                    thecell = thesheet.GetCell(cell.ColumnIndex, cell.RowIndex);
+                    thelistofcells[i++] = thecell;
+                }
+                thesheet.AddUndo(thelistofcells);                                        // log change in undo stack
+                foreach (DataGridViewCell cell in dataGridView1.SelectedCells)                      // update cells
+                {
+                    thecell = thesheet.GetCell(cell.ColumnIndex, cell.RowIndex);
+                    thecell.BGColor = (uint)changecolor.Color.ToArgb();
+                }
+                thesheet.ClearRedoStack();                                                          // clear redo stack
+                this.Undo.Enabled = true;                                                           // enable undo button
+                this.Redo.Enabled = false;                                                          // disable redo button
+                this.Undo.Text = "Undo Background Color Change";
+            }
+        }
+
+        //**************************************** UNDO/REDO *******************************************************
+
+        private void Undo_Click(object sender, EventArgs e)             // undo button
+        {
+            Undo.Text = thesheet.PopUndoStack();                        // execute undo
+            if (thesheet.UndoIsEmpty() == true)
+            { this.Undo.Enabled = false; }                              // disable undo button if undo stack is empty
+            this.Redo.Enabled = true;                                   // enable redo button
+            Redo.Text = thesheet.PeekRedoStack();                       // update redo text
+
+        }
+
+        private void Redo_Click(object sender, EventArgs e)             // redo button
+        {
+            Redo.Text = thesheet.PopRedoStack();                        // execute redo
+            if (thesheet.RedoIsEmpty() == true)
+            { this.Redo.Enabled = false; }                              // disable redo button if redo stack is empty
+            this.Undo.Enabled = true;                                   // enable undo button
+            Undo.Text = thesheet.PeekUndoStack();                       // update undo text
+        }
+
+
+
+        //*************************************** SAVE/LOAD **********************************************************
+        private void Save_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.SaveFileDialog saveit = new SaveFileDialog(); // Helper code found on MSDN and Stackoverflow
+            saveit.Title = "Save File";
+            saveit.Filter = "txt files (*.txt)|*.txt| xml files (*.xml)|*.xml| All files (*.*)|*.*";
+            saveit.FilterIndex = 2; // set default to txt file
+            saveit.RestoreDirectory = true;
+
+            if (saveit.ShowDialog() == DialogResult.OK)
+            {
+                if ((saveit.FileName) != null)
+                {
+                    using (Stream thestream = saveit.OpenFile())
+                    {
+                    thesheet.SaveSpreadsheet(thestream);
+                    thestream.Dispose();
+                    }
+                }
+            }
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog openit = new OpenFileDialog();
+            openit.Title = "Open File"; // dialog
+            openit.Filter = "txt files (*.txt)|*.txt| xml files (*.xml)|*.xml"; // limit to text and xml files
+            openit.FilterIndex = 2;
+            openit.RestoreDirectory = true; // return to original directory after close ... i think
+            Undo.Enabled = false;
+            Redo.Enabled = false;
+
+            if (openit.ShowDialog() == DialogResult.OK) // OK button is clicked in dialog
+            {
+                using (Stream thestream = openit.OpenFile())
+                {
+                    thesheet.LoadSpreadsheet(thestream);
+                    thestream.Dispose();
+                }
+            }
+        }
+    }
+}
